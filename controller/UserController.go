ac3532f8503041fdba14cbd603e6a00326924714
RdotZz <3210817013@qq.com>
2023-04-25 19:54:46 +0800

    version V3 Login with bcrpyt加密

--- controller/UserController.go
+++ controller/UserController.go
@@ -6,6 +6,7 @@ import (
 	"OceanLearn/util"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
@@ -40,16 +41,61 @@ func Register(context *gin.Context) {
 		return
 	}
 	//创建用户
+	//同时加密用户密码,给给定Cost返回密码的bcrypt哈希，默认cost为10，cost为哈希次数
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密错误"})
+	}
 	newUser := model.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasedPassword),
 	}
 	DB.Create(&newUser)
 
 	//返回结果
 	context.JSON(200, gin.H{
+		"code":    200,
 		"message": "注册成功",
+	})
+}
+func Login(context *gin.Context) {
+	DB := common.GetDB()
+	telephone := context.PostForm("telephone")
+	password := context.PostForm("password")
+
+	if len(telephone) != 11 {
+		//gin.h == type H map[string]any
+		context.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位置"})
+		return
+	}
+	if len(password) < 6 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		return
+	}
+
+	//判断手机号是否存在
+	var user model.User
+	DB.Where("telephone = ? ", telephone).First(&user)
+	if user.ID == 0 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
+		return
+	}
+	//判断密码
+	//bcrypt加密：加盐单向加密，不可逆加密算法，每次加密后密文都不一样
+	//CompareHashAndPassword：对比bcrypt哈希字符串和密码明文是否匹配
+	//bcrypt.CompareHashAndPassword(密文，明文密码)
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "密码错误"})
+		return
+	}
+	//发放token
+	token := "11"
+	//返回结果
+	context.JSON(200, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登录成功",
 	})
 }
 
--- router.go
+++ router.go
@@ -7,5 +7,7 @@ import (
 
 func CollectRoute(r *gin.Engine) *gin.Engine {
 	r.POST("/api/auth/register", controller.Register)
+	r.POST("/api/auth/Login", controller.Login)
+
 	return r
 }
